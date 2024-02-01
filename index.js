325f6a2be0ad3a6e70cdb8c91b3127605e12e91c
unknown <rai2889@gmail.com>
2024-02-01 16:56:53 +0530

    bck

---
+++ index.js
@@ -0,0 +1,40 @@
+const express=require("express")
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/catogries");
+const connection= require("./config/db")
+const app=express()
+const cors=require("cors")
+const mongoose = require("mongoose");
+const multer = require("multer");
+app.use(express.json())
+
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.body.name);
+    },
+  });
+
+  const upload = multer({ storage: storage });
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+  });
+
+  app.use("/api/auth", authRoute)
+  app.use("/api/users", userRoute);
+  app.use("/api/posts", postRoute);
+  app.use("/api/categories", categoryRoute);
+app.listen(8000, async()=>{
+    try {
+        await connection
+        console.log("server running at port 8000")
+    } catch (error) {
+        console.log(error)
+    }
+})
