fb7a6fcc01db95ba714a9d2e11539eb252a1e5dd
unknown <rai2889@gmail.com>
2024-02-07 23:26:44 +0530

    d

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const categoryRoute = require("./routes/catogries");
 const connection= require("./config/db")
 const app=express()
 const cors=require("cors")
-app.use(express.static("images"));
+app.use("/images", express.static("images"));
+
 
 const mongoose = require("mongoose");
 const multer = require("multer");
